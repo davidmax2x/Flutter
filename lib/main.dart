900e10c3962b69a24682f1b5242f5f3f7c6e12d8
davidmax2x <118828077+davidmax2x@users.noreply.github.com>
2023-06-21 06:10:00 -0700

    "added a few bits of component"

--- lib/main.dart
+++ lib/main.dart
@@ -11,34 +11,82 @@ class WelcomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body:
+    return SafeArea(
+      child: Scaffold(
+      backgroundColor: Colors.blue,
+       body:
       
-       Center(
-         child: 
-        Container(
-          margin: const EdgeInsets.all(70),
-            color: Colors.white,
-            child:const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text("HELLO!"),
-                SizedBox(height: 20,),
-                Divider(height: 5,),
-                TextField(
-                  keyboardType: TextInputType.name,
-                  decoration: InputDecoration(
-                    prefixIcon:Icon(Icons.person),
+        Center(
+          child: Container(
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(30),
+              
+              color: Colors.white,
+              ),
+            padding:const EdgeInsets.fromLTRB(20,30,20,0),
+            margin: const EdgeInsets.fromLTRB(0,200,0,0 ),
+              
+              
+              child:
+              Column(
+                children:  [
+                  const Text("LOGIN",style: TextStyle(
+                    fontSize: 20,
+                  ),),
+                  const SizedBox(height: 40,),
+                  const TextField(
+                    keyboardType: TextInputType.name,
+                    
+                    decoration: 
+                    InputDecoration(
+                    hintText: "USERNAME",
+                    prefixIcon: Icon(Icons.person),
+                    hintStyle: TextStyle(
+                      fontWeight: FontWeight.w300
+                    ),
+                    border: OutlineInputBorder(),
+                
+                    ),
+                  ),
+                  const SizedBox(height: 20,),
+                  const TextField(
+                    obscureText: true,
+                    keyboardType: TextInputType.visiblePassword,
+                    decoration: InputDecoration(
+                    
+                    hintText: "PASSWORD",
+                    prefixIcon: Icon(Icons.key),
+                    hintStyle: TextStyle(
+                      fontWeight: FontWeight.w300
+                    ),
+                    border: OutlineInputBorder()
+                    ),
+                  ),
+                  const SizedBox(height: 20,),
+                  TextButton(onPressed: (){}, 
+                  style: const ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(Colors.blue),
+                    
+                  ),
+                  child: const Text("LOGIN",style: TextStyle(color: Colors.white),)),
+                const SizedBox(height: 10,),
+                Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("Already have an account?"),
+                      const SizedBox(width: 10,),
+                      InkWell(
+                        onTap: (){},
+                        highlightColor: Colors.red,
+                        child: const Text("Register here"),),
+                    ],
                   ),
                 )
-              ],
-            )
-            
-             ),
-         ),
-       ); 
+                ],)
+              
+            ),
+        )));
+        
     
   }
 }
