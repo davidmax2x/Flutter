8deeae8caef7de7219ba61ee4c55f17f9774e770
davidmax2x <118828077+davidmax2x@users.noreply.github.com>
2023-06-24 05:15:23 -0700

    working on registeration page

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -7,21 +7,64 @@ class Register extends StatelessWidget {
   Widget build(BuildContext context) {
     return  SafeArea(
       child: Scaffold(
+        resizeToAvoidBottomInset: false,
         backgroundColor: Colors.blue,
         body:
          Center(
           child: 
           Container(
             padding: const EdgeInsets.fromLTRB(20, 30, 20, 10),
+            margin: const EdgeInsets.fromLTRB(30, 30, 30, 35),
             decoration: BoxDecoration(borderRadius: BorderRadius.circular(30),
             color: Colors.white
             ),
             
             child: 
             const Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+              
               children: [
-                TextField()
+                Text("Register",style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.blue,
+                ),),
+                SizedBox(height: 30,),
+                TextField(
+                  keyboardType: TextInputType.name,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Full Name",
+                    isCollapsed: false,
+                  ),
+                ),
+                SizedBox(height: 20,),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Phone Number",
+                    isCollapsed: false,
+                  ),
+                ),
+                SizedBox(height: 20,),
+                TextField(
+                  obscureText: true,
+                  keyboardType: TextInputType.visiblePassword,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Password",
+                    isCollapsed: false,
+                  ),
+                ),
+                SizedBox(height: 20,),
+                TextField(
+                  obscureText: true,
+                  keyboardType: TextInputType.visiblePassword,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Confirm Password",
+                    isCollapsed: false,
+                  ),
+                )
               ],
             ),
           ),
