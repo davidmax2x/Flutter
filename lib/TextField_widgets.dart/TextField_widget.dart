8275cf31ee3be4b8014f738ce53e2265b8032794
davidmax2x <118828077+davidmax2x@users.noreply.github.com>
2023-07-21 08:37:08 -0700

    hi

---
+++ lib/TextField_widgets.dart/TextField_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TextFields extends StatelessWidget {
+  const TextFields({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const TextField(
+        
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,9 @@ import 'package:flutter_application_1/screens/register_screen.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home:WelcomePage() ,
-    debugShowCheckedModeBanner:false ,));
+    home: WelcomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
 
 class WelcomePage extends StatelessWidget {
@@ -13,106 +14,109 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        resizeToAvoidBottomInset: false,
-        
-      backgroundColor: Colors.blue,
-       body:
-        PageView(
-          
-          scrollDirection: Axis.vertical,
-          children: [Center(
-            child: Container(
-              decoration: BoxDecoration(borderRadius: BorderRadius.circular(30),
-                color: Colors.white,
-                ),
-              padding:const EdgeInsets.fromLTRB(20,30,20,0),
-              margin: const EdgeInsets.fromLTRB(0,200,0,0 ),
-                
-                
-                child:
-                Column(
-                  children:  [
-                    const Text("LOGIN",style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold
-                    ),),
-                    const SizedBox(height: 40,),
-                    const TextField(
-                      keyboardType: TextInputType.emailAddress,
-                      
-                      decoration: 
-                      InputDecoration(
-                      hintText: "Email Address",
-                      prefixIcon: Icon(Icons.person),
-                      prefixIconColor: Colors.blue,
-                      hintStyle: TextStyle(
-                        fontWeight: FontWeight.w300
-                      ),
-                      border: OutlineInputBorder(),
-                  
-                      ),
+        child: Scaffold(
+            resizeToAvoidBottomInset: false,
+            backgroundColor: Colors.blue,
+            body: SingleChildScrollView(
+              
+              child: Center(
+                child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: Colors.white,
                     ),
-        
-                    const SizedBox(height: 20,),
-        
-                    const TextField(
-                      obscureText: true,
-                      keyboardType: TextInputType.visiblePassword,
-                      decoration: InputDecoration(
-                      hintText: "PASSWORD", 
-                      prefixIcon: Icon(Icons.key),
-                      hintStyle: TextStyle(
-                        fontWeight: FontWeight.w300
-                      ),
-                      prefixIconColor: Colors.blue,
-                      border: OutlineInputBorder()
-                      ),
-                    ),
-        
-                    const SizedBox(height: 20,),
-        
-                   const Align(
-                      alignment: Alignment.bottomLeft,
-                      child: Text("Forgot password",style: TextStyle(
-                        color: Colors.blue,
-                        fontSize: 17,
-                      ),),
-                    ),
-                    const  SizedBox(height: 20,),
-                    MaterialButton(onPressed: (){},
-                    color: Colors.blue,
-                    minWidth: 170.0,
-                     
-                    child: const Text("LOGIN",style: TextStyle(color: Colors.white),)),
-                  const SizedBox(height: 30,),
-        
-                  Center(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                    padding: const EdgeInsets.fromLTRB(20, 30, 20, 0),
+                    margin: const EdgeInsets.fromLTRB(0, 200, 0, 0),
+                    child: Column(
                       children: [
-                        const Text("Already have an account?"),
-                        const SizedBox(width: 10,),
-                        InkWell(
-                          onTap: (){
-                            Navigator.of(context).push(MaterialPageRoute(builder: (context) => const register(),));
-                          },
-                          
-                          child: const Text("Register here",style: TextStyle(
-                            color: Color.fromRGBO(25, 118, 210, 1),
-                          ),),),
+                        const Text(
+                          "LOGIN",
+                          style: TextStyle(
+                              fontSize: 25, fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(
+                          height: 40,
+                        ),
+                        const TextField(
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                            hintText: "Email Address",
+                            prefixIcon: Icon(Icons.person),
+                            prefixIconColor: Colors.blue,
+                            hintStyle: TextStyle(fontWeight: FontWeight.w300),
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        const TextField(
+                          obscureText: true,
+                          keyboardType: TextInputType.visiblePassword,
+                          decoration: InputDecoration(
+                              hintText: "PASSWORD",
+                              prefixIcon: Icon(Icons.key),
+                              hintStyle: TextStyle(fontWeight: FontWeight.w300),
+                              prefixIconColor: Colors.blue,
+                              border: OutlineInputBorder()),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        const Align(
+                          alignment: Alignment.bottomLeft,
+                          child: Text(
+                            "Forgot password",
+                            style: TextStyle(
+                              color: Colors.blue,
+                              fontSize: 17,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        MaterialButton(
+                            onPressed: () {},
+                            color: Colors.blue,
+                            minWidth: 170.0,
+                            child: const Text(
+                              "LOGIN",
+                              style: TextStyle(color: Colors.white),
+                            )),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        Center(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text("Already have an account?"),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              InkWell(
+                                onTap: () {
+                                  Navigator.of(context).push(MaterialPageRoute(
+                                    builder: (context) => const register(),
+                                  ));
+                                },
+                                child: const Text(
+                                  "Register here",
+                                  style: TextStyle(
+                                    color: Color.fromRGBO(25, 118, 210, 1),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
                       ],
-                    ),
-                  ),
-        
-                  const SizedBox(height: 30,),
-                  
-                  ],)
-                
+                    )),
               ),
-          ),
-        ],)));
-        
-    
+            )));
   }
 }
